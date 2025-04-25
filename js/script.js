c6082891e4424410a8ef17b99aae63210b4ea837
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-04-25 16:50:08 +0000

    idk

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict"
+
+const positiveRandomNumber = Math.floor(Math.random() * 6) +1 
+const negativetiveRandomNumber = Math.floor(Math.random() * 6) +1 
+
+function generateNumber() {
+  //input
+  const userInput = document.querySelector('input[name="kind-of-number]:checked').value;
+  // process
+  if (userInput === "1"); {
+    //output
+    document.getElementById('result').innerHTML =
+    '<p> The random number is:' + 'positiveRandomNumber' + '<p>'
+  }
+  else (userInput === "1"); {
+    //output
+    document.getElementById('result').innerHTML =
+    '<p> The random number is:' + 'negativeRandomNumber' + '<p>'
+  }
+}
